ef111266975817cebedea5b1aee8ad09b89b2734
Rai Butera <rai@rbutera.com>
2019-02-11 15:36:25 +0000

    fixed missing jquery error message

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = (baseConfig, env, defaultConfig) => {
+  // Extend defaultConfig as you need.
+
+  // For example, add typescript loader:
+  defaultConfig.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    include: path.resolve(__dirname, '../src'),
+    loader: require.resolve('ts-loader')
+  });
+
+  defaultConfig.module.rules.push({
+    test: /\.scss$/,
+    loaders: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../')
+  });
+
+  defaultConfig.resolve.extensions.push('.ts', '.tsx');
+
+  defaultConfig.plugins.push(
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery'
+    })
+  );
+
+  return defaultConfig;
+};
