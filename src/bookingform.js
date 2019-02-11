2aa350f2eed516b34115001e3549a71476d08255
Rai Butera <rai@rbutera.com>
2019-02-11 13:01:28 +0000

    added storybook and bootstrap

---
+++ src/bookingform.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class BookingForm extends Component {
+  render() {
+    return <div>Booking Form</div>;
+  }
+}
+
+export default BookingForm;
---
+++ stories/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { BookingForm } from "../src/bookingform";
+
+storiesOf("Booking Form", module).add("Event Enquiry", () => <BookingForm />);
